bc017e17143482acd323bc2c98d06d35a3580685
William de Bouter <97745377+Yowcry@users.noreply.github.com>
2023-08-29 11:52:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const targetDate = new Date(2022, 0, 1)
+const yearsElement = document.querySelector('.years')
+const daysElement = document.querySelector('.days')
+const hoursElement = document.querySelector('.hours')
+const minutesElement = document.querySelector('.minutes')
+const secondsElement = document.querySelector('.seconds')
+
+const onStart = () => {
+    setTimeout(function() {
+        document.querySelector('#countup').classList.remove('hidden')
+    }, 2000)
+}
+
+const onTick = ({ years, days, hours, minutes, seconds }) => {
+    yearsElement.innerHTML = years
+    daysElement.innerHTML = days
+    hoursElement.innerHTML = hours
+    minutesElement.innerHTML = minutes
+    secondsElement.innerHTML = seconds
+}
+
+const options = new LsCountupOptions({ targetDate, onTick, onStart })
+const countup = new LsCountup(options)
+
+countup.start()
